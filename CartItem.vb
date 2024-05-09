1eb13f502039f3c0bbd1b625462564ec8b218084
Lhamster12 <167773259+Lhamster12@users.noreply.github.com>
2024-05-09 10:26:35 +0800

    Add files via upload

---
+++ CartItem.vb
@@ -0,0 +1,15 @@
+﻿Namespace midterm_project
+    Public Class CartItem
+        Public Property Name As String
+        Public Property Price As Decimal
+        Public Property Quantity As Integer
+        Public Property Total As Decimal
+
+        Public Sub New(ByVal Name As String, ByVal Price As Decimal, ByVal Quantity As Integer)
+            Me.Name = Name
+            Me.Price = Price
+            Me.Quantity = Quantity
+            Me.Total = Price * Quantity
+        End Sub
+    End Class
+End Namespace
